70f6fee3dd79a93b0c1707a433081a6f2758f9b0
David Gustafsson <david.gustafsson@ub.gu.se>
2016-09-14 13:36:25 +0200

    Fix wrong provisioning file and use fake production ip for now to avoid accidents

--- bin/provision-production.sh
+++ bin/provision-production.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bash "$SCRIPT_DIR/pre-provision.sh production"
-ansible-playbook -i "$SCRIPT_DIR/../hosts" $SCRIPT_DIR/../pre-provisioning/playbook.yml
+ansible-playbook -i "$SCRIPT_DIR/../hosts" $SCRIPT_DIR/../provisioning/playbook.yml
